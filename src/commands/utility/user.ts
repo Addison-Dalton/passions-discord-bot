ba555116e62d0ad8d4f2b65b73d54894295efeb1
Addison Dalton <zarch14@gmail.com>
2024-06-17 21:03:01 -0400

    removed tutorial commands

--- src/commands/utility/user.ts
+++
@@ -1,9 +0,0 @@
-import { SlashCommandBuilder, CommandInteraction } from "discord.js";
-
-export const data = new SlashCommandBuilder()
-    .setName("user")
-    .setDescription("Replies with user info!");
-
-export async function execute(interaction: CommandInteraction) {
-    await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
-}
